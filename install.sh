adb2f16682eb5ee8da305aef4820ea5de7f16d39
Ayoub Elmouafik <ayoubmf514@gmail.com>
2025-09-01 17:38:47 +0100

    update in script

--- install.sh
+++ install.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
+if command -v docker &> /dev/null
+then
+    echo "✅ Docker is already installed."
+    docker --version
+else
+    echo "Docker not found. Starting installation..."
 
-sudo apt-get update
-sudo apt-get install -y ca-certificates curl gnupg
+    sudo apt-get update
+    sudo apt-get install -y ca-certificates curl gnupg
 
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+    sudo install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
+    echo \
+      "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+      "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-sudo apt-get update
+    sudo usermod -aG docker $USER
 
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-
-sudo usermod -aG docker $USER
-
-
-echo ""
-echo "✅ Docker installed successfully!"
+    echo ""
+    echo "✅ Docker installed successfully!"
+fi
