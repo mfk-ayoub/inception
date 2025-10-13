237943244b971a60c7bfd920391f6510c33da14f
mfk-ayoub <ayoubmf514@gmail.com>
2025-10-13 19:08:16 +0100

    update

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+set -e
 
+mkdir -p /etc/nginx/ssl
 
-nginx -g "daemon off;"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+-keyout /etc/nginx/ssl/server.key \
+-out /etc/nginx/ssl/server.crt \
+-subj="/C=MA/ST=benguerir/L=benguerir/O=1337 School/OU=ayel-mou/CN=ayel-mou.42.fr"
+
+exec nginx -g "daemon off;"
