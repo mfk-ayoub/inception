1d2e9f83fa591102859899ff867a573e264e0ca3
mfk-ayoub <ayoubmf514@gmail.com>
2025-10-14 16:42:01 +0100

    update

--- srcs/requirements/mariadb/tools/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-sudo service mariadb start
-mysql -u root -p
-
-mariadb -e  "CREATE DATABASE test"
---
+++ srcs/requirements/nginx/tools/generate-ssl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+
+set -e
+
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=en/ST=IDF/L=bg/O=1337/OU=1337/CN=ayel-mou.1337.ma"
+
+exec  nginx -g "daemon off"
