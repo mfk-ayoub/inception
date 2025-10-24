a3466aeaf2c15afecb7846ca95e281b971751fdd
mfk-ayoub <ayoubmf514@gmail.com>
2025-10-24 03:42:26 +0100

    update

--- srcs/requirements/nginx/tools/setup_ssl.sh
+++ srcs/requirements/nginx/tools/setup_ssl.sh
@@ -1,7 +1,9 @@
-
-
 #!/bin/bash
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=en/ST=IDF/L=bg/O=1337/OU=1337/CN=ayel-mou.1337.ma"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout /etc/ssl/private/nginx-selfsigned.key \
+    -out /etc/ssl/certs/nginx-selfsigned.crt \
+    -subj "/C=ma/ST=IDF/L=bg/O=1337/OU=1337/CN=ayel-mou.1337.ma"
 
-nginx -g "daemon off;"
+
+exec nginx -g "daemon off;"
