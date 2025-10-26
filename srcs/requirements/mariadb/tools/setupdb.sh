838605a4c6854dbcc89d80e99fafd7b1fc6221ec
mfk-ayoub <ayoubmf514@gmail.com>
2025-10-26 05:43:07 +0100

    update

--- srcs/requirements/mariadb/tools/setupdb.sh
+++ srcs/requirements/mariadb/tools/setupdb.sh
@@ -4,30 +4,34 @@ set -e
 chown -R mysql:mysql /var/run/mysqld
 chown -R mysql:mysql /var/lib/mysql
 
-first_time=0
-if [ ! -f "/var/lib/mysql/.initialized" ]; then
-  touch /var/lib/mysql/.initialized
-  first_time=1
-fi
-
-service mariadb start
-
 ROOT_PASSWORD="$(cat /run/secrets/db_root_password)"
 WORDPRESS_DB_PASSWORD="$(cat /run/secrets/db_password)"
 
-while ! mysqladmin ping ; do
-    sleep 2
-done
+first_time=0
+if [ ! -f "/var/lib/mysql/.initialized" ]; then
+  first_time=1
 
-if [ "$first_time" = "1" ]; then
-  mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASSWORD}';"
+  mysql_install_db --user=mysql --datadir=/var/lib/mysql --auth-root-authentication-method=normal
+
+
+  mysqld --user=mysql &
+
+  while ! mysqladmin ping -u root ; do
+      sleep 2
+  done
+
+  mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASSWORD}';"
+
+  mysql -uroot -p"${ROOT_PASSWORD}" <<EOF
+CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+CREATE USER IF NOT EXISTS '${WORDPRESS_DB_USER}'@'%' IDENTIFIED BY '${WORDPRESS_DB_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${WORDPRESS_DB_USER}'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+  mysqladmin -u root -p"${ROOT_PASSWORD}" shutdown
+
+  touch /var/lib/mysql/.initialized
 fi
-
-mysql -uroot -p"${ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
-mysql -uroot -p"${ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS '${WORDPRESS_DB_USER}'@'%' IDENTIFIED BY '${WORDPRESS_DB_PASSWORD}';"
-mysql -uroot -p"${ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${WORDPRESS_DB_USER}'@'%';"
-mysql -uroot -p"${ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
-
-mysqladmin -u root -p"${ROOT_PASSWORD}" shutdown
 
 exec mysqld
